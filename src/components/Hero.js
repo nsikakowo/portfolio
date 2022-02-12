b010effa97eb232bba5ff7bb357836215fd3bee4
nsikakowo <owonsikak@gmail.com>
2022-02-12 11:40:05 +0100

    introducing effects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,103 @@
 import React from 'react';
+import Particles from "react-tsparticles";
 import './style.css'
 
+
+
 const Hero = () => {
+
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
       <main className='hero-tab'>
-          <div className="hero-bg"></div>
+          <div className="hero-bg">
+          <Particles
+          className='particles'
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.9,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+          detectRetina: true,
+        }
+            }}
+    />
+          </div>
           <h1>Hi, I'm Nsikak OwO.</h1>
           <p>Frontend Developer</p>
       </main>
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import pdf from './NsikakOwo.pdf'
+import logo from '../logo.png'
 import './style.css'
 
 const Topbar = () => {
   return (
       <main className='topbar'>
           <h1 className="logo">
-              Nsikak OwO
+             <img src={logo} alt="logo" width='170px'/>
           </h1>
-          <div className="download-btn">
-              <button>Download My Full Resume</button>
+          <div className="download-btn" ><a href={pdf} target='_blank' rel='noreferrer'>
+              <button>Download My Full Resume</button></a>
           </div>
       </main>
   );
