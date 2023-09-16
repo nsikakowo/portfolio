db8a29213d7eee5a1c911fa5671eed3a58119df6
nsikak owo <52787942+nsikakowo@users.noreply.github.com>
2023-09-16 15:58:52 +0100

    Update Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -9,9 +9,9 @@ const Summary = () => {
           <h3 className="tab-heading">PROFESSIONAL SUMMARY</h3>
           <p className="summary-body">
          Experienced frontend engineer with a strong background of over 9years in the computer software industry. Skilled in problem-solving using HTML, CSS, TailwindCSS, JavaScript, React, NextJs, Shopify, WordPress and other Content management systems. Currently exploring the React Native and Web3 pathways.
-
+<br/>
 Passionate about teamwork and believe in the collective growth of the team. Committed to being a valuable contributor to any project, always willing to lend a hand to fellow team members when needed.
-
+<br/>
 Experienced in collaborating with scrum teams, participating in sprint planning, backlog refinement, and other scrum ceremonies.
           </p>
       </main>
